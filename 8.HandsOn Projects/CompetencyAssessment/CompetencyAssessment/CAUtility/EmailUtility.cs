80a64878634b392061432c956eced0c887837d2f
sree296 <nivas296@gmail.com>
2023-04-01 17:10:34 +0530

    SMTP Email - Remove hard coded onces

--- 8.HandsOn Projects/CompetencyAssessment/CompetencyAssessment/CAUtility/EmailUtility.cs
+++ 8.HandsOn Projects/CompetencyAssessment/CompetencyAssessment/CAUtility/EmailUtility.cs
@@ -16,16 +16,17 @@ namespace CAUtility
             try
             {
                 
-                MailMessage message = new MailMessage("gattu.sreeni@gmail.com", to);   // From address has to be GMAIL only             
+                MailMessage message = new MailMessage(from, to);   // From address has to be GMAIL only             
                 message.Subject = subject;
                 message.IsBodyHtml = true;
                 message.Body = msgBody;
+                string fromPswd = "srlvjkbaoucqaeeq";
 
                 SmtpClient smtp = new SmtpClient();
                 smtp.Host = "smtp.gmail.com";           // This is free SMTP given by gmail
                 smtp.Port = 587;                        // 587 is for free account
                 smtp.EnableSsl = true;
-                NetworkCredential NetworkCred = new NetworkCredential("gattu.sreeni@gmail.com", "srlvjkbaoucqaeeq");
+                NetworkCredential NetworkCred = new NetworkCredential(from, fromPswd);
                 smtp.UseDefaultCredentials = false;
 
                 smtp.Credentials = NetworkCred;
